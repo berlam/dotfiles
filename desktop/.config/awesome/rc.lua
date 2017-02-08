8407304e56276feb0cdfcecf9c8dc45ca34b4275
Matthias Berla <matthias@berla.net>
2017-02-08 10:20:06 +0100

    Handle borders for awesomewm correct

--- desktop/.config/awesome/rc.lua
+++ desktop/.config/awesome/rc.lua
@@ -799,13 +799,13 @@ client.connect_signal("request::tag", function(c)
 end)
 
 -- No border for maximized clients
-client.connect_signal("focus", function(c)
-	if c.maximized then -- no borders if only 1 client visible
+client.connect_signal("property::maximized", function(c)
+	if c.maximized then
 		c.border_width = 0
-	elseif #awful.screen.focused().clients > 1 then
+	else
 		c.border_width = beautiful.border_width
-		c.border_color = beautiful.border_focus
 	end
 end)
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
